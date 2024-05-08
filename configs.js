632c76bd8f6724cd1df6000f4153a31fcbc57855
Geronimo Concha <concha.geronimojose@iesconcepcion.edu.ar>
2024-05-08 07:15:45 -0300

    primer commit

--- configs.js
+++ configs.js
@@ -1,15 +1,22 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import mysql from 'mysql2/promise';
-
-const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Gero4316%',
-    database: 'salvadora'
-});
+import dotenv from 'dotenv';
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename)
+const __dirname = dirname(__filename);
+const PASSWORD_DB = process.env.PASSWORD_DB;
+const NAME_DB = process.env.NAME_DB;
+const USSER_DB = process.env.USSER_DB;
+const HOST_DB = process.env.HOST_DB;
+const PORT = process.env.PORT;
 
-export { __filename, __dirname, path, connection }
+const connection = await mysql.createConnection({
+    host: HOST_DB,
+    user: USSER_DB,
+    password: PASSWORD_DB,
+    database: NAME_DB
+});
+
+export { __filename, __dirname, path, connection, PORT }
--- index.js
+++ index.js
@@ -1,18 +1,20 @@
 import express from 'express';
-const app = express()
+import { PORT } from './configs.js';
 
 import staticFile from "./middleware/staticFile.js";
 import engine from './middleware/engine.js';
 import views from './middleware/views.js';
 
+import index from './routes/index.js';
+
+const app = express()
+
 staticFile(app);
 engine(app);
 views(app);
 
-import index from './routes/index.js';
-
 app.use('/',index);
 
-app.listen(3000,()=>{
+app.listen(PORT,()=>{
     console.log('Listen on port 3000');
 })
